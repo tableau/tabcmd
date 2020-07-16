aa3e45f78f003f3352b9118f8b9ce8b0a7dcd8bf
Ovini Nanayakkara <onanayakkara@tableau.com>
2020-07-16 15:26:35 -0700

    Updated setup.py file to include test folder

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     url='https://github.com/tableau/tabcmd2',
     packages=['pythontabcmd2', 'pythontabcmd2.commands', 'pythontabcmd2.parsers',
               'pythontabcmd2.tableauserverclient'],
-    test_suite='tests',
+    test_suite='Tests',
     install_requires=[
         'requests>=2.11,<3.0',
         'urllib3>=1.24.3,<2.0',
