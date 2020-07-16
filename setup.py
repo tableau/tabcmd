d28c4765147c855f9d8f028bc9529efef991ca7e
Ovini Nanayakkara <onanayakkara@tableau.com>
2020-07-16 15:19:54 -0700

    Updated yml file

---
+++ setup.py
@@ -0,0 +1,23 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='pythontabcmd2',
+    url='https://github.com/tableau/tabcmd2',
+    packages=['pythontabcmd2', 'pythontabcmd2.commands', 'pythontabcmd2.parsers',
+              'pythontabcmd2.tableauserverclient'],
+    test_suite='tests',
+    install_requires=[
+        'requests>=2.11,<3.0',
+        'urllib3>=1.24.3,<2.0',
+        'dill>=0.3'
+    ],
+    tests_require=[
+        'requests-mock>=1.0,<2.0',
+        'pytest',
+        'mock',
+        'unittests'
+    ]
+)
