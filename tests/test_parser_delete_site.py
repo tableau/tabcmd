69999dd25cd8e1229c2e5a94b36330734fc7192d
Ovini Nanayakkara <onanayakkara@tableau.com>
2020-08-27 16:50:18 -0700

    delete site parser test updated

--- tests/test_parser_delete_site.py
+++ tests/test_parser_delete_site.py
@@ -22,17 +22,6 @@ class DeleteSiteParserTest(unittest.TestCase):
         assert args == mock_args.return_value
 
     @mock.patch('argparse.ArgumentParser.parse_args',
-                return_value=argparse.Namespace(site_name=None,
-                                                username="test",
-                                                password="testpass",
-                                                server="http://test",
-                                                site="helloworld"))
-    def test_delete_project_required_name_none(self, mock_args):
-        args = DeleteSiteParser.delete_site_parser()
-        assert args == mock_args.return_value
-        assert args.site_name == mock_args.return_value.site_name
-
-    @mock.patch('argparse.ArgumentParser.parse_args',
                 return_value=argparse.Namespace())
     def test_delete_project_missing_args(self, mock_args):
         with self.assertRaises(AttributeError):
