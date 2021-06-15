2986cb27f9937af8835df346a54fd601a00a03aa
russgresh-kazoo <russ@playkazoo.com>
2021-06-15 09:32:16 -0400

    added a function to handle pdf orientation and page type options in the export command and improved the error reporting

--- pythontabcmd2/commands/datasources_and_workbooks/export_command.py
+++ pythontabcmd2/commands/datasources_and_workbooks/export_command.py
@@ -40,6 +40,40 @@ class ExportCommand(DatasourcesAndWorkbooks):
             print("error")
         return '{}/sheets/{}'.format(separated_list[0], separated_list[1])
 
+    def get_pdf_request_options(self):
+        # handle orientation and page type options
+        req_option_pdf = TSC.PDFRequestOptions(maxage=1)
+        if self.args.pagelayout == 'landscape':
+            req_option_pdf.orientation = TSC.PDFRequestOptions.Orientation.Landscape
+        else:
+            req_option_pdf.orientation = TSC.PDFRequestOptions.Orientation.Portrait
+            
+        if self.args.pagesize == 'a3':
+            req_option_pdf.page_type = TSC.PDFRequestOptions.PageType.A3
+        elif self.args.pagesize == 'a4':
+            req_option_pdf.page_type = TSC.PDFRequestOptions.PageType.A4
+        elif self.args.pagesize == 'a5':
+            req_option_pdf.page_type = TSC.PDFRequestOptions.PageType.A5
+        elif self.args.pagesize == 'b5':
+            req_option_pdf.page_type = TSC.PDFRequestOptions.PageType.B5
+        elif self.args.pagesize == 'executive':
+            req_option_pdf.page_type = TSC.PDFRequestOptions.PageType.Executive
+        elif self.args.pagesize == 'folio':
+            req_option_pdf.page_type = TSC.PDFRequestOptions.PageType.Folio
+        elif self.args.pagesize == 'ledger':
+            req_option_pdf.page_type = TSC.PDFRequestOptions.PageType.Ledger
+        elif self.args.pagesize == 'legal':
+            req_option_pdf.page_type = TSC.PDFRequestOptions.PageType.Legal
+        elif self.args.pagesize == 'note':
+            req_option_pdf.page_type = TSC.PDFRequestOptions.PageType.Note
+        elif self.args.pagesize == 'quarto':
+            req_option_pdf.page_type = TSC.PDFRequestOptions.PageType.Quarto
+        elif self.args.pagesize == 'tabloid':
+            req_option_pdf.page_type = TSC.PDFRequestOptions.PageType.Tabloid
+        else:
+            req_option_pdf.page_type = TSC.PDFRequestOptions.PageType.Letter
+        return req_option_pdf
+        
     def export(self, server):
         """Method to export using Tableauserverclient methods"""
         if self.args.fullpdf:  # its a workbook
@@ -47,9 +81,7 @@ class ExportCommand(DatasourcesAndWorkbooks):
             try:
                 workbook_from_list = self.get_request_option_for_workbook(
                     server, workbook)
-
-                req_option_pdf = TSC.PDFRequestOptions(maxage=1)
-
+                req_option_pdf = self.get_pdf_request_options()
                 server.workbooks.populate_pdf(workbook_from_list,
                                               req_option_pdf)
                 if self.args.filename is None:
@@ -62,7 +94,7 @@ class ExportCommand(DatasourcesAndWorkbooks):
                     self.logger.info("Exported successfully")
 
             except TSC.ServerResponseError as e:
-                self.logger.error("Server error occurred")
+                self.logger.error("Server error occurred: {}".format(e))
 
         if self.args.pdf or self.args.png or self.args.csv:  # its a workbook
             if self.args.pdf:  # its a view
@@ -71,7 +103,7 @@ class ExportCommand(DatasourcesAndWorkbooks):
                     views_from_list = self.get_request_option_for_view(
                         server, view)
 
-                    req_option_pdf = TSC.PDFRequestOptions(maxage=1)
+                    req_option_pdf = self.get_pdf_request_options()
 
                     server.views.populate_pdf(views_from_list,
                                               req_option_pdf)
@@ -86,7 +118,7 @@ class ExportCommand(DatasourcesAndWorkbooks):
                         self.logger.info("Exported successfully")
 
                 except TSC.ServerResponseError as e:
-                    self.logger.error("Server error occurred")
+                    self.logger.error("Server error occurred: {}".format(e))
             if self.args.csv:
                 view = self.get_view(self.url)
                 try:
@@ -107,7 +139,7 @@ class ExportCommand(DatasourcesAndWorkbooks):
                         self.logger.info("Exported successfully")
 
                 except TSC.ServerResponseError as e:
-                    self.logger.error("Server error occurred")
+                    self.logger.error("Server error occurred: {}".format(e))
             if self.args.png:
                 view = self.get_view(self.url)
                 try:
@@ -126,4 +158,4 @@ class ExportCommand(DatasourcesAndWorkbooks):
                         self.logger.info("Exported successfully")
 
                 except TSC.ServerResponseError as e:
-                    self.logger.error("Server error occurred")
+                    self.logger.error("Server error occurred: {}".format(e))
