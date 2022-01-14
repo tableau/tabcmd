4eaa1d3e9602f29b7b6823aac08b2c26d9def9e3
Ovini Nanayakkara <ovini.nanayakkara@ovinina-ltm5cuo.internal.salesforce.com>
2022-01-14 07:42:40 -0800

    print all commands complete

--- pythontabcmd2/tabcmd2_controller.py
+++ pythontabcmd2/tabcmd2_controller.py
@@ -5,7 +5,6 @@ from .logger_config import get_logger
 from .parsers.help_parser import HelpParser
 from .map_of_commands import *
 
-
 logger = get_logger('pythontabcmd2.tabcmd2_controller', 'info')
 
 
@@ -18,18 +17,22 @@ class Tabcmd2Controller:
             HelpParser.print_help_description()
             Tabcmd2Controller.get_list_of_commands_available()
             parser.print_help()
-            print("PRINT ALL COMMANDS")
-            sys.exit()
-        elif len(sys.argv) == 1 or sys.argv[1]:
-            HelpParser.print_help_description()
-            parser.print_help()
             sys.exit()
         parser.add_argument('command', nargs='?', default="help")
         args = parser.parse_args(sys.argv[1:2])
         if args.command is not None:
             return args.command
+        elif len(sys.argv) == 1 or sys.argv[1]:
+            HelpParser.print_help_description()
+            parser.print_help()
+            sys.exit()
 
     @classmethod
     def get_list_of_commands_available(cls):
-        for command in CommandsMap.commands_hash_map:
-            print(command)
+        for command, command_tuple in CommandsMap.commands_hash_map.items():
+            Tabcmd2Controller.print_formatted_list(command, command_tuple[1])
+
+    @classmethod
+    def print_formatted_list(cls, command, description_of_command):
+
+        print(" %-20s %-15s %-5s" % (command, "--", description_of_command))
