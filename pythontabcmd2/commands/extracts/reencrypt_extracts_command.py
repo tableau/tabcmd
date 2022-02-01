2ffac9d4817900193f3fe5d959b3fe49f7ba739a
Ovini Nanayakkara <44311587+ovinis@users.noreply.github.com>
2022-02-01 14:04:07 -0500

    Extractscode (#36)
    * test
    
    * updated extracts commands
    
    * create extracts command
    
    * Docstrings for extracts command
    
    * fixed pycodestyle for extracts
    
    * Removed print statements
    
    * Updates to extracts commands
    
    * Moved common methods to CommonCommand class

--- pythontabcmd2/commands/extracts/reencrypt_extracts_command.py
+++ pythontabcmd2/commands/extracts/reencrypt_extracts_command.py
@@ -2,10 +2,19 @@ import tableauserverclient as TSC
 from .. import log
 from ... import Session
 from .. import ReencryptExtractsParser
+from ..extracts.extracts_command import ExtractsCommand
+from ..site.site_command import SiteCommand
 
 
-class ReencryptExtracts:
+class ReencryptExtracts(ExtractsCommand):
+    """
+    Command to Reencrypt all extracts on a site with new encryption keys.
+    This command will regenerate the key encryption key and
+    data encryption key. You must specify a site.
+    """
+
     def __init__(self, args, site_name):
+        super().__init__(args)
         self.site_name = site_name
         self.args = args
         self.logging_level = args.logging_level
@@ -23,4 +32,10 @@ class ReencryptExtracts:
         self.reencrypt_extract(server_object)
 
     def reencrypt_extract(self, server):
-        pass
+        try:
+            site_id = SiteCommand.find_site_id(server, self.site_name)
+            job = server.sites.encrypt_extracts(site_id)
+            self.logger.info("Extract re-encryption started with "
+                             "JobID: {}".format(job.id))
+        except TSC.ServerResponseError as e:
+            self.logger.error('Server Error', e)
