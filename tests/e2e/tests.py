1a18953dff49a95cef61a7d66dd83f1ea094dc8e
Jac Fitzgerald <jac.fitzgerald@salesforce.com>
2022-03-04 17:34:00 -0800

    extract sleep time to var

--- tests/e2e/tests.py
+++ tests/e2e/tests.py
@@ -4,6 +4,9 @@ import time
 from tests.e2e import setup_e2e
 from tests.e2e import vars
 
+debug_log = "--logging-level=DEBUG"
+indexing_sleep_time = 1  # wait 1 second to confirm server has indexed updates
+
 
 def _test_command(test_args: list[str]):
     # this will raise an exception if it gets a non-zero return code
@@ -11,6 +14,11 @@ def _test_command(test_args: list[str]):
     calling_args = [setup_e2e.exe] + test_args
     print(calling_args)
     return subprocess.check_call(calling_args)
+
+
+def test_login():
+    setup_e2e.prechecks()
+    setup_e2e.login()
 
 
 def test_create_delete_group():
@@ -23,12 +31,6 @@ def test_create_delete_group():
     command = "deletegroup"
     arguments = [command, vars.group_name]
     _test_command(arguments)
-
-
-def test_login():
-    setup_e2e.prechecks()
-    setup_e2e.login()
-
 
 def test_create_delete_project():
     project_name = vars.project_name
@@ -47,7 +49,7 @@ def test_create_delete_project():
     arguments = [command, "--name", project_name, "--parent-project-path", parent_path]
     _test_command(arguments)
 
-    time.sleep(1)
+    time.sleep(indexing_sleep_time)
 
     # project 3
     parent_path = "{0}/{1}".format(project_name, project_name)
@@ -55,14 +57,14 @@ def test_create_delete_project():
     arguments = [command, "--name", project_name, "--parent-project-path", parent_path]
     _test_command(arguments)
 
-    time.sleep(1)
+    time.sleep(indexing_sleep_time)
 
     # delete project 2 (containing 3)
     command = "deleteproject"
     arguments = [command, project_name, "--parent-project-path", project_name]
     _test_command(arguments)
 
-    time.sleep(1)
+    time.sleep(indexing_sleep_time)
 
     # delete project 1
     command = "deleteproject"
@@ -77,3 +79,8 @@ def test_list_sites(self):
     except Exception as e:
         # often won't have permission for sites
         print(e)
+
+def test_create_extract(self):
+    command = "createextract"
+    arguments = [command, "-d", "Regional", "--project", "Samples", "--encrypt"]
+    _test_command(arguments)
