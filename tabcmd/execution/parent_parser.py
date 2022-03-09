791f26a38d9a3bccbc8fed199a7ef2f0bfb82aa7
Jac Fitzgerald <jac.fitzgerald@salesforce.com>
2022-03-08 23:06:14 -0800

    fix defaults
    setting a default here was overriding my handling of it in Session.py - removed defaults from the parser setup

--- tabcmd/execution/parent_parser.py
+++ tabcmd/execution/parent_parser.py
@@ -2,6 +2,7 @@ import argparse
 
 
 class ParentParser:
+    # Ref https://docs.python.org/3/library/argparse.html
     """Parser that will be inherited by all commands. Contains
     authentication and logging level setting"""
 
@@ -24,20 +25,21 @@ class ParentParser:
     def parent_parser_with_global_options(self):
         parser = argparse.ArgumentParser(usage=argparse.SUPPRESS, add_help=False)
 
-        # The default behavior is to try certificates in the computer store, like a browser?
-        # There is no option to say 'use defaults'
         certificates = parser.add_mutually_exclusive_group()
         certificates.add_argument(
             "-c",
             "--use-certificate",
             dest="certificate",
+            default=None,
             metavar="",
-            help="Use client certificate to sign in. Required when mutual SSL is enabled.",
+            help="Use client certificate to sign in. Required when mutual SSL is enabled. "
+                 "(The default behavior is to try certificates from the store)",
         )
         certificates.add_argument(
             "--no-certcheck",
             action="store_true",
-            help="When specified, tabcmd (the client) does not validate the server's SSL certificate.",
+            help="When specified, tabcmd (the client) does not validate the server's SSL certificate. "
+                 "(The default behavior is to try certificates from the store)",
         )
 
         cookies = parser.add_mutually_exclusive_group()
@@ -59,7 +61,7 @@ class ParentParser:
             choices=["DEBUG", "INFO", "ERROR"],
             default="info",
             metavar="",
-            help="Use the specified logging level. If not specified, the default level is INFO.",
+            help="Use the specified logging level. The default level is INFO.",
         )
 
         parser.add_argument("--no-prompt", action="store_true", help="no prompt for password")
@@ -68,6 +70,7 @@ class ParentParser:
         auth_options.add_argument(
             "-tn",
             "--token-name",
+            default=None,
             metavar="<TOKEN NAME>",
             help="The name of the Tableau Server Personal Access Token. If using a token to sign in,\
                   this is required at least once to begin session.",
@@ -75,6 +78,7 @@ class ParentParser:
         auth_options.add_argument(
             "-u",
             "--username",
+            default=None,
             metavar="<USER>",
             help="Use the specified Tableau Server username. For Tableau Online, this will be an email address.",
         )
@@ -90,11 +94,13 @@ class ParentParser:
         secret_values.add_argument(
             "-p",
             "--password",
+            default=None,
             metavar="<PASSWORD>",
             help="Use the specified Tableau Server password. Requires --username to be set.",
         )
         secret_values.add_argument(
             "--password-file",
+            default=None,
             metavar="<FILE>",
             help="Read the password from the given .txt file rather than the command line for increased security.",
         )
@@ -104,27 +110,27 @@ class ParentParser:
             "-x",
             "--proxy",
             dest="proxy",
+            default=None,
             metavar="<HOST:PORT>",
             help="Connect to Tableau Server using the specified HTTP proxy.",
         )
         proxy_group.add_argument(
             "--no-proxy",
             action="store_false",
-            dest="proxy",
             help="Do not use a HTTP proxy.",
         )  # is this the default behavior?
 
         parser.add_argument(
             "-s",
             "--server",
-            default="http://localhost",
+            default=None,  # default is handled in Session class
             metavar="<URL>",
             help="Use the specified Tableau Server URL. If no protocol is specified, http:// is assumed.",
         )
         parser.add_argument(
             "-t",
             "--site",
-            default="",
+            default=None,  # default is handled in Session class
             metavar="SITEID",
             help='Use the specified Tableau Server site. Leave empty or specify an empty string ("") to \
                     force use of the default site',
@@ -132,6 +138,7 @@ class ParentParser:
 
         parser.add_argument(
             "--timeout",
+            default=None,  # default is handled in Session class
             metavar="<SECONDS>",  # can't use -t, it's already used for --site
             help="How long to wait, in seconds, for the server to complete processing the command. The default \
                     behavior is to wait until the server responds.",
