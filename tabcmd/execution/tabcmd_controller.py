1611f61940d49729fe8e6f644063e461ec497413
Jac Fitzgerald <jac.fitzgerald@salesforce.com>
2022-03-17 14:21:12 -0700

    simplify execution flow

--- tabcmd/execution/tabcmd_controller.py
+++ tabcmd/execution/tabcmd_controller.py
@@ -1,14 +1,13 @@
 from .map_of_commands import *
 from .map_of_parsers import *
 from .parent_parser import ParentParser
-
-"""
-This is where we go through all the parsers and manually attach them to a command
-"""
+import sys
 
 
 class TabcmdController:
-    def initialize_parsers(self):
+
+    @staticmethod
+    def initialize():
         manager = ParentParser()
         parent = manager.get_root_parser()
 
@@ -16,5 +15,30 @@ class TabcmdController:
         parsers = ParsersMap.parsers_hashmap
         for commandname in commands.keys():
             parsers[commandname](manager, commands[commandname])
-
         return parent
+
+    # during normal execution, leaving input as none will default to sys.argv
+    # for testing, we want to be able to pass in different updates
+    @staticmethod
+    def run(parser, input=None):
+
+        if input is None and len(sys.argv) <= 1:  # no arguments given
+            parser.print_help()
+            sys.exit(0)
+        namespace = parser.parse_args(input)
+        if not namespace.func:  # arguments did not match any command
+            parser.print_help()
+            sys.exit(1)
+
+        # maybe argparse will do this?
+        if namespace.func == "help":
+            print("argparse?")
+            # parser.print_help()
+            # sys.exit(0)
+
+        # if a subcommand was identified, call the function assigned to it
+        # this is the functional equivalent of the call by reflection in the previous structure
+        # https://stackoverflow.com/questions/49038616/argparse-subparsers-with-functions
+        namespace.func(namespace).run_command(namespace)
+        return namespace
+
