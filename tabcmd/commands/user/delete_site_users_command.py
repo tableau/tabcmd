0a2b04daf8b1dd5b60c326d7eb54e6fa4772619e
Jac Fitzgerald <jac.fitzgerald@salesforce.com>
2022-04-15 00:30:34 -0700

    delete_site_users should only work on current site
    also move find_user_id into try-catch block

--- tabcmd/commands/user/delete_site_users_command.py
+++ tabcmd/commands/user/delete_site_users_command.py
@@ -30,12 +30,7 @@ class DeleteSiteUsersCommand(Server):
         session = Session()
         server = session.create_session(args)
 
-        if args.site_name:
-            target_site = args.site_name
-        else:
-            target_site = "current site"
-
-        logger.info("Deleting users listed in {0} from site '{1}'".format(args.filename.name, target_site))
+        logger.info("Deleting users listed in {0} from current site".format(args.filename.name))
 
         UserCommand.validate_file_for_import(args.filename, logger, strict=args.require_all_valid)
         number_of_users_deleted = 0
@@ -47,19 +42,16 @@ class DeleteSiteUsersCommand(Server):
 
         error_list = []
         for user_obj in user_obj_list:
-            user_id = UserCommand.find_user_id(logger, server, user_obj.name)
             try:
+                user_id = UserCommand.find_user_id(logger, server, user_obj.name)
                 server.users.remove(user_id)
                 logger.info("Successfully deleted user from site: {}".format(user_obj.name))
                 number_of_users_deleted += 1
-            except TSC.ServerResponseError as e:
-                Errors.check_common_error_codes_and_explain(logger.info, e)
+            except Exception as e:
+                Errors.check_common_error_codes_and_explain(logger, e)
                 number_of_errors += 1
                 error_list.append(e)
-            except ValueError:
-                logger.error(" Could not delete user: User {} not found".format(user_obj.name))
-                error_list.append(e)
-                number_of_errors += 1
+
         logger.info("======== 100% complete ========")
         logger.info("======== Number of users deleted from site: {} =========".format(number_of_users_deleted))
         logger.info("======== Number of errors {} =========".format(number_of_errors))
