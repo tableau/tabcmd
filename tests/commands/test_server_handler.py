652daca686697637440e06ab27ae82a80d45e7b0
Jac Fitzgerald <jac.fitzgerald@salesforce.com>
2022-04-15 00:16:40 -0700

    formatting

--- tests/commands/test_server_handler.py
+++ tests/commands/test_server_handler.py
@@ -5,25 +5,22 @@ from tabcmd.commands.constants import Errors
 
 # TODO add checks that the logger was called?
 class ServerTests(unittest.TestCase):
-
     def test_exit_with_logger_problem(self):
         try:
             Errors.exit_with_error(None, "a message appears")
         except SystemExit as um:
-            assert(um.code == 1)
+            assert um.code == 1
 
     def test_exit_with_message(self):
         try:
             log = logging.getLogger("exitwithmessage")
             Errors.exit_with_error(log, "a message appears")
         except SystemExit as um:
-            assert(um.code == 1)
-
+            assert um.code == 1
 
     def test_exit_with_exception(self):
         try:
             log = logging.getLogger("exitwithexception")
             Errors.exit_with_error(log, "a message appears")
         except SystemExit as um:
-            assert(um.code == 1)
-
+            assert um.code == 1
