3dc64d014e34dc5e54a991d38e752871b5db93a1
Jac <jacalata@users.noreply.github.com>
2022-05-05 06:17:41 -0700

    Jac/add testing (#103)
    * add test coverage:  increase test output, add tests for execution code, add test ordering for e2e tests

--- tests/commands/test_execution.py
+++ tests/commands/test_execution.py
@@ -2,22 +2,30 @@ import argparse
 import sys
 import unittest
 import mock
-
+from tabcmd.execution.logger_config import *
 from tabcmd.execution.tabcmd_controller import TabcmdController
 
 
 class ExecutionTests(unittest.TestCase):
     @mock.patch("sys.argv", [""])
-    def test_run_e2e(self):
+    def test_launch(self):
         parser = TabcmdController.initialize()
         TabcmdController.run(parser, ["help"])
         # check exit code = 0?
 
-    def test_run_initialize(self):
+    def test_initialize(self):
         TabcmdController.initialize()
 
     @mock.patch("sys.argv", "")
-    def test_run_inputs(self):
+    def test_inputs(self):
         fake_parser = mock.MagicMock(argparse.ArgumentParser)
         with self.assertRaises(SystemExit):
             TabcmdController.run(fake_parser, None)
+
+    def test_config_logger(self):
+        configure_log("log_name", "DEBUG")
+        configure_log("log_name", "INFO")
+        configure_log("log_name", "ERROR")
+
+    def test_get_logger(self):
+        log("testing", "DEBUG")
