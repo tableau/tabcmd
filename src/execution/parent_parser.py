77e6bff0ebb351cd284aba9513563f263a7736cc
Jac Fitzgerald <jac.fitzgerald@salesforce.com>
2022-06-22 22:14:43 -0700

    configure black
    add options in pyproject.toml so you just run 'black .'
    update contributors

--- src/execution/parent_parser.py
+++ src/execution/parent_parser.py
@@ -1,8 +1,10 @@
 import argparse
 from .localize import _
+
 # when we drop python 3.8, this could be replaced with this lighter weight option
 # from importlib.metadata import version, PackageNotFoundError
 from pkg_resources import get_distribution, DistributionNotFound
+
 try:
     version = get_distribution("tabcmd").version
 except DistributionNotFound:
