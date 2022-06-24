b518881b084d473051467af0327f9b41a3e498d0
Jac <jacalata@users.noreply.github.com>
2022-06-24 13:45:04 -0700

    configure black (#133)
    * configure black
    * add step to list dependencies for licensing

--- bin/e2e.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# not tested as a script
-pytest -q tests\e2e\online_tests.py -r pfE
--- tabcmd.py
+++ tabcmd.py
@@ -1,4 +1,4 @@
 from src import tabcmd
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     tabcmd.main()
