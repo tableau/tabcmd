933d27c361adae30a41c69b249255773b96ac873
Jac Fitzgerald <jac.fitzgerald@salesforce.com>
2023-03-11 15:16:39 -0800

    update error output on fatal crash

--- tabcmd/__main__.py
+++ tabcmd/__main__.py
@@ -3,9 +3,8 @@ import sys
 try:
     from tabcmd.tabcmd import main
 except ImportError as e:
-    print(sys.stderr, e)
-    print(sys.stderr, "Tabcmd needs to be run as a module, it cannot be run as a script")
-    print(sys.stderr, "Try running python -m tabcmd")
+    print("Exception thrown running program: `{}`, `{}`".format(e, e.__context__), file=sys.stderr)
+    print("[Possible cause: Tabcmd needs to be run as a module, try running `python -m tabcmd`]", file=sys.stderr)
     sys.exit(1)
 
 if __name__ == "__main__":
