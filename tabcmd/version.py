79c142b2f2af1b565cf5745c7aaf17a04426e35d
Jac Fitzgerald <jac.fitzgerald@salesforce.com>
2025-10-07 22:19:15 -0700

    mypy

--- tabcmd/version.py
+++ tabcmd/version.py
@@ -1,8 +1,7 @@
-# Updated to use importlib.metadata (available in Python 3.8+, required >= 3.9)
-from importlib.metadata import version, PackageNotFoundError
+import importlib.metadata as libs
 
 try:
-    version = version("tabcmd")
-except PackageNotFoundError:
+    version = libs.version("tabcmd")
+except libs.PackageNotFoundError:
     version = "2.0.0"
     pass
